94dad88979b322b13462d7884630759f05b85f0a
Andy Hill <andy@andyhill.us>
2015-02-18 21:36:14 -0500

    cleaned up a bit

--- index.php
+++ index.php
@@ -8,14 +8,12 @@ $local = [
 ];
 
 $page = new \Athill\Utils\Page($local);
-$h->h2($site['pagetitle']);
 
-$h->p('content');
-$h->a('http://andyhill.us', 'andyhill.us');
 
+//// move to heading
 $menu = new \Athill\Utils\MenuUtils('/nested/nest2/nest2.2/nest2.2.1.php');
 $breadcrumbs = $menu->getBreadcrumbs();
-$h->pa($breadcrumbs);
+// $h->pa($breadcrumbs);
 $h->onav('id="breadcrumbs"');
 $lastbc = count($breadcrumbs) - 1;
 $delim = '&gt;';
@@ -32,8 +30,11 @@ foreach ($breadcrumbs as $i => $breadcrumb){
 }
 $h->ctag('ul');
 $h->cnav('/#breadcrumbs');
-$h->onav('id="top-menu"');
+$h->onav('id="top-menu" class="clearfix"');
 $menu->renderMenu();
 $h->cnav('.#top-menu');
+//// content
+$h->h2($site['pagetitle']);
+$h->p('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sit amet tellus ultricies, malesuada ex non, porta odio. Nunc elementum bibendum vestibulum. Cras commodo tincidunt libero at suscipit. Vestibulum suscipit justo in pellentesque blandit. Vivamus hendrerit est lobortis eros aliquet bibendum. Ut sodales diam nibh, et tempor magna posuere ac. In tristique efficitur ornare. Phasellus pretium, sapien eu placerat vehicula, felis ex placerat sapien, vel venenatis felis felis non leo. Vivamus lorem lacus, consequat id euismod interdum, mollis a augue. ');
 
 $page->end();
