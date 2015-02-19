ac2b7cbef53aad97ccbf76d2f45b8bfee11cb21e
Andy Hill <andy@andyhill.us>
2015-02-18 22:02:51 -0500

    working on file generation

--- index.php
+++ index.php
@@ -37,4 +37,35 @@ $h->cnav('.#top-menu');
 $h->h2($site['pagetitle']);
 $h->p('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sit amet tellus ultricies, malesuada ex non, porta odio. Nunc elementum bibendum vestibulum. Cras commodo tincidunt libero at suscipit. Vestibulum suscipit justo in pellentesque blandit. Vivamus hendrerit est lobortis eros aliquet bibendum. Ut sodales diam nibh, et tempor magna posuere ac. In tristique efficitur ornare. Phasellus pretium, sapien eu placerat vehicula, felis ex placerat sapien, vel venenatis felis felis non leo. Vivamus lorem lacus, consequat id euismod interdum, mollis a augue. ');
 
+
+$menu = json_decode(file_get_contents('menu.json'), true);
+
+renderMenu();
 $page->end();
+
+
+	function renderMenu($options=[]) {
+		global $h, $menu;
+		$defaults = [
+			//// these change with recursion
+			'menu'=>$menu,
+			'currdepth' => 0,
+			'buildpath'=>''
+		];
+		$options = $h->extend($defaults, $options);
+		foreach ($options['menu'] as $entry) {
+			$href = $options['buildpath'].$entry['href'];
+			if ($href != '/') {
+				$h->tbr($href);
+				if (isset($entry['children'])) {
+					$change = $h->extend($options, [
+						'menu'=>$entry['children'],
+						'currdepth'=>$options['currdepth']+1,
+						'buildpath'=>$options['buildpath'].$entry['href']
+					]);
+					renderMenu($change);
+				} 				
+			}
+
+		}
+	}
