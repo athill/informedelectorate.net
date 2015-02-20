4bfc9d1fca4ddc54c8dcd1859262d8e2a0173581
Andy Hill <andy@andyhill.us>
2015-02-19 22:12:11 -0500

    progress on creating a directory structure. heredocs don't seem to be working.

--- index.php
+++ index.php
@@ -38,34 +38,61 @@ $h->h2($site['pagetitle']);
 $h->p('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sit amet tellus ultricies, malesuada ex non, porta odio. Nunc elementum bibendum vestibulum. Cras commodo tincidunt libero at suscipit. Vestibulum suscipit justo in pellentesque blandit. Vivamus hendrerit est lobortis eros aliquet bibendum. Ut sodales diam nibh, et tempor magna posuere ac. In tristique efficitur ornare. Phasellus pretium, sapien eu placerat vehicula, felis ex placerat sapien, vel venenatis felis felis non leo. Vivamus lorem lacus, consequat id euismod interdum, mollis a augue. ');
 
 
-$menu = json_decode(file_get_contents('menu.json'), true);
 
-renderMenu();
+
+////// generate directory structure
+$menu = json_decode(file_get_contents('menu.json'), true);
+$template = <<<EOT
+	&lt;?php
+EOT;
+
+
+// require_once(%s.'setup.inc.php');
+// \$local = [
+// ];
+
+// \$page = new \Athill\Utils\Page(\$local);
+
+// \$page->end();
+
+// $template = '';
+generateFileStructure(['menu'=>$menu, 'template'=>$template]);
 $page->end();
 
 
-	function renderMenu($options=[]) {
-		global $h, $menu;
-		$defaults = [
-			//// these change with recursion
-			'menu'=>$menu,
-			'currdepth' => 0,
-			'buildpath'=>''
-		];
-		$options = $h->extend($defaults, $options);
-		foreach ($options['menu'] as $entry) {
-			$href = $options['buildpath'].$entry['href'];
-			if ($href != '/') {
-				$h->tbr($href);
+
+function generateFileStructure($options=[]) {
+	global $h, $site;
+	$defaults = [
+		'template'=>'',
+		//// these change with recursion
+		'menu'=>'',
+		'currdepth' => 0,
+		'buildpath'=>''
+	];
+	$options = $h->extend($defaults, $options);
+	foreach ($options['menu'] as $entry) {
+		$href = $options['buildpath'].$entry['href'];
+		if ($href != '/') {
+			$filepath = $site['fileroot'].$href;
+			if (preg_match('/\.php$/', $href)) {
+				$h->tbr('file: '.$filepath);
+				$h->tbr(sprintf($options['template'], str_repeat('../', $options['currdepth'])));
+
+			} else {
+				$h->tbr('dir: '.$filepath);
+				$h->tbr(sprintf($options['template'], str_repeat('../', $options['currdepth'])));				
 				if (isset($entry['children'])) {
 					$change = $h->extend($options, [
 						'menu'=>$entry['children'],
 						'currdepth'=>$options['currdepth']+1,
 						'buildpath'=>$options['buildpath'].$entry['href']
 					]);
-					renderMenu($change);
-				} 				
+					generateFileStructure($change);
+				} 					
 			}
-
+			
 		}
+
 	}
+}
