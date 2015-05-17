6e8c0f3881e0433f1a9253247feec9a50b067b90
Andy Hill <andy@andyhill.us>
2015-05-17 16:30:04 -0400

    reps is working

---
+++ classes/Api/Api.php
@@ -0,0 +1,55 @@
+<?php namespace Classes\Api;
+class Api {
+	protected $url = '';
+	protected $key = '';
+	public $separator = '.json';
+	protected $debug = false;
+
+	function __construct($key='') {
+		if ($key !== '') $this->key = $key;
+	}
+
+	protected function getUrl($method, $params=array()) {
+		$url = $this->url.$method.$this->separator.'?apikey='.$this->key;
+		// echo $url;
+		foreach ($params as $k => $v) {
+			if (!is_array($v)) {
+				$url .= '&'.urlencode($k).'='.urlencode($v);
+			} else {
+				foreach ($v as $value) {
+					$url .= '&'.urlencode($k).'='.urlencode($value);
+				}
+				//$url .= '&all_legislators=1';
+			}
+		}
+		 // echo $url;
+		return $url;
+	}
+
+	public function get($method, $params=array()) {
+		$url = $this->getUrl($method, $params);
+		$content = $this->getResponse($url);
+		// $content = file_get_contents($url);
+		if ($this->debug) {
+			echo $url.'<br>';
+			echo $content.'<br><br>';			
+		}
+
+		
+		return json_decode($content, true);
+	}
+
+	function getResponse($url) {
+		// $ch = curl_init();
+		// curl_setopt($ch, CURLOPT_URL, $url);
+		// curl_setopt($ch, CURLOPT_HEADER, false);
+		// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		// // curl_setopt($ch, CURLOPT_VERBOSE, true);
+		// $content = curl_exec($ch);
+		$content = file_get_contents($url);
+		echo $url;
+		return $content;		
+	}
+
+}
+?>
---
+++ classes/Api/Sunlight.php
@@ -0,0 +1,55 @@
+<?php  namespace Classes\Api;
+class Sunlight extends Api {
+
+	public function getData($collection, $method, $params=array()) {
+		echo 'api: '.$this->api;
+		switch ($collection) {
+			//// deprecated
+			case 'congress':
+				$this->url = 'http://services.sunlightlabs.com/api/';
+				////http://services.sunlightlabs.com/api/api.method.format?apikey=YOUR_API_KEY&<params>
+				break;
+			case 'congress3':
+				$this->url = 'https://congress.api.sunlightfoundation.com';
+				$this->separator = '/';
+				break;
+			case 'openstates':
+				$this->url = 'http://openstates.org/api/v1';
+				$this->separator = '/';
+				////http://openstates.org/api/v1/bills/?{SEARCH-PARAMS}&apikey={YOUR_API_KEY}
+				break;
+			case 'capitolwords':
+				$this->url = 'http://capitolwords.org/api/';
+				////http://capitolwords.org/api/dates.json?apikey=<YOUR_KEY>
+				break;
+			case 'transparencydata':
+				$this->url = 'http://transparencydata.com/api/1.0/';
+				////http://transparencydata.com/api/1.0/<method>.<format>
+				break;
+			//// deprecated
+			case 'realtime':
+			default:
+				$this->url = 'https://api.realtimecongress.org/api/v1/';
+				break;
+		}
+		return parent::get($method, $params);
+
+	}
+
+	public function getFederalLegislatorsByLatLong($lat, $long) {
+		$data = $this->getData('congress3', '/legislators/locate', array('latitude'=>$lat,'longitude'=>$long));
+		return $data;
+	}
+	public function getStateLegislatorsByLatLong($lat, $long) {
+		$data = $this->getData('openstates', '/legislators/geo', array('lat'=>$lat,'long'=>$long));
+		return $data;
+	}
+
+	function getFullName($d) {
+		$name = $d['first_name'];
+		if ($d['middle_name'] != '') $name .= ' '.$d['middle_name'];
+		if ($d['nickname'] != '') $name .= ' ('.$d['nickname'].')';
+		$name .= ' '.$d['last_name'];		
+		return $name;
+	}
+}
