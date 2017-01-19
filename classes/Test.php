b397cfd42f2497ff1e19849fd9582c2d9a35dbba
Andy Hill <andy@andyhill.us>
2017-01-19 17:36:27 -0500

    clean slate

--- classes/Test.php
+++
@@ -1,8 +0,0 @@
-<?php namespace Classes;
-
-class Test {
-	function __construct() {
-		echo "I'm testing away. Set a coding course for the // TODO: destination that rhymes with 'virgin sea'";
-	}
-
-}
