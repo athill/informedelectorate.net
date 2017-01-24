37748915e4c360ed55b872e4be6d9d9922eaf86e
Andy Hill <andy@andyhill.us>
2017-01-23 20:40:19 -0500

    working on statetax, wrapping head around d3 in es6

---
+++ resources/assets/js/pages/statetax.js
@@ -0,0 +1,153 @@
+import * as d3 from "d3";
+import d3tip from "d3-tip";
+import { geoAlbersUsa, geoPath } from "d3-geo";
+import { parse } from 'd3-request';
+
+//// retrieve data files asynchronously
+const promises = [
+	fetch('/data/statetax/data.json'),
+	fetch('/data/statetax/states.json'),
+];
+
+Promise.all(promises).then(
+	(data, geoData) => {
+		$(() => {
+			app(data, geoData);
+		});
+	    // returned data is in arguments[0], arguments[1], ... arguments[n]
+	    // you can process it here
+	}, 
+	err => {
+    	// error occurred
+    	console.err('oops', err);
+	}
+);
+
+var w = 400;
+var h = 250;
+
+//// props
+// https://derekswingley.com/2016/08/01/using-and-bundling-individual-d3-modules/
+
+const app = (data, states) => {
+	const max = {};		//// maximum values for primary areas
+	const scales = {};	//// scaling functions by area
+	const colorcode = {	//// color multipliers
+		r: 0.2,
+		g: 1,
+		b: 0.1
+	};
+	const dollars = d3.format('00');		//// format function for dollars
+	const width = 400;
+	const height = 250;
+	//// build max map
+	Object.keys(data).forEach(state => {
+		Object.keys(data[state]).forEach(type => {
+			if (!(type in max) || parseInt(data[state][type]) > parseInt(max[type])) {
+				max[type] = data[state][type];
+			}
+		});
+	});
+
+
+	//// Build basic scaling functions
+	Object.keys(max).forEach(type => {
+		scales[type] = d3.scale.linear()
+			.domain([0, max[type]])
+			.range([255, 0])
+	});
+	//// set up tooltip
+	const tip = d3tip()
+	  .attr('class', 'd3-tip')
+	  .offset([0, -10])
+	  .html(function(d) {
+	  	const area = $('input[name=option]:checked').val();
+	  	const name = d.properties.NAME;
+	    return getTooltip(name, area);
+	});
+
+	//Define map projection
+	const projection = geoAlbersUsa()
+	   .translate([w/2, h/2])
+	   .scale([500]);	  
+
+	//Define path generator
+	const path = geoPath()
+		.projection(projection);
+
+	//// area
+	const area = $('input[name=option]:checked').val();
+	
+	//Create SVG element
+	const svg = d3.select("#state_map")
+				.append("svg")
+				.attr("width", w)
+				.attr("height", h);
+	//// Initialize tooltip
+	svg.call(tip);
+
+	//Load in GeoJSON data
+	parse(states, function(json) {
+		
+		//Bind data and create one path per GeoJSON feature
+		svg.selectAll("path")
+		   .data(json.features)
+		   .enter()
+		   .append("path")
+		   .attr("d", path)
+		   .attr('class', 'state')
+		   .attr('id', function(d) { return d.properties.NAME; })
+		   .style("fill", function(d) {
+		   		var name = d.properties.NAME;
+		   		return getRgb(name, area)
+		   	})
+		   .on('mouseover', tip.show)
+		   .on('mouseout', tip.hide)
+	});
+
+	//// Change option
+	$('#interface-container').on('click', 'input[name=option]', function(e)  {
+		var area = $('input[name=option]:checked').val();
+		var $states = $states || $('.state');
+		$states.each(function(i, elem) {
+			var name = $(this).attr('id');
+			$(this).css('fill', getRgb(name, area));
+		});
+	});		
+}
+
+const getRgb = (name, area) => {
+	if (!(name in data)) return {};
+	var rgb = {};
+	var areas = area.split('+');
+	//// create area in scales if it doesn't exist
+	if (!(area in scales)) {
+		var mx = areas.reduce(function(p, c) { 
+			return p + parseInt(max[c]); 
+		}, 0);
+		scales[area] = d3.scale.linear()	
+							.domain([0, mx])
+							.range([255, 0]);
+	}
+	//// build rgb
+	var value = areas.reduce(function(p, c) {
+		return p + parseInt(data[name][c]);
+	}, 0);
+	for (var color in colorcode) {
+		rgb[color] = Math.floor(scales[area](value)*colorcode[color]);
+	}
+	var rgbstr = 'rgb('+rgb.r+','+rgb.g+','+rgb.b+')';
+	return rgbstr;
+}
+
+const getTooltip = (name, area) => {
+	var value = name;
+	if (name in data) {
+		var areas = area.split('+');
+		var total = areas.reduce(function(p, c) {
+			return p + parseInt(data[name][c]);
+		}, 0);
+		value += ' - ' + dollars(total);
+	} 
+	return value;	
+}
