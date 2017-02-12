d4fe162d083ac688b7ccb112546871ddd20aaa42
Andy Hill <andy@andyhill.us>
2017-02-12 17:06:54 -0500

    utils: add constants for network failure alert and loading icon, add method to create jquery table structure; floorupdates: use new items in utils

--- resources/assets/js/pages/floorupdates.js
+++ resources/assets/js/pages/floorupdates.js
@@ -1,21 +1,19 @@
 import { ucFirst } from 'change-case';
 import moment from 'moment';
 
+import { NETWORK_FAILURE_ALERT, LOADING_ICON, getTableObject } from '../utils';
+
 $(() => {
 	const $results = $('#results');
-	$results.html('Loading results <i class="fa fa-cog fa-spin fa-2x fa-fw"></i>')
+	$results.html('Loading results '+LOADING_ICON);
 	fetch('/api/floorupdates')
 		.then(response => response.json())
 		.then(json => {
-			const $tablewrap = $('<div class="table-responsive" />');
-			const $table = $('<table class="table table-hover table-border" />');
-			$tablewrap.append($table);
-			const $thead = $('<thead />');
-			['Date', 'Chamber', 'Event', 'Bills'].forEach(header => $thead.append(`<th>${header}</th>`));
-			const $tbody = $('<tbody />');
-			$table.append($thead, $tbody);
-			$tablewrap.append($table);
-			$results.html($tablewrap);
+			//// build table structure
+			const headers = ['Date', 'Chamber', 'Event', 'Bills'];
+			const { $tbody, $table } = getTableObject(headers);
+			$results.html($table);
+			//// populate table with data
 			json.forEach(update => {
 				const $tr = $('<tr />');
 				$tr.append('<td>'+moment(update.timestamp).format("MMMM Do YYYY, h:mm:ss a")+'</td>');
@@ -32,8 +30,10 @@ $(() => {
 				$td.append($bills);
 				$tr.append($td);
 				$tbody.append($tr);
-
 			});
 		})
-		.catch(error => console.error('Error in floorupdates', error));
+		.catch(error => {
+			$results.html(NETWORK_FAILURE_ALERT);
+			console.error('Error in floorupdates', error)
+		});
 });
--- resources/assets/js/utils/index.js
+++ resources/assets/js/utils/index.js
@@ -1,4 +1,8 @@
 import moment from 'moment';
+import $ from 'jquery';
+
+export const NETWORK_FAILURE_ALERT = '<div class="alert alert-danger" role="alert">Something went wrong, please try again later.</div>';
+export const LOADING_ICON = '<i class="fa fa-cog fa-spin fa-2x fa-fw"></i>';
 
 export const formatDate = date => {
 	return moment(date).format("MMMM Do YYYY, h:mm:ss a");
@@ -15,3 +19,21 @@ export function getParameterByName(name, url) {
     if (!results[2]) return '';
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 };
+
+export function getTableObject(headers) {
+    const $tablewrap = $('<div class="table-responsive" />');
+    const $table = $('<table class="table table-hover" />');
+    $tablewrap.append($table);
+    const $tr = $('<tr />');
+    const $thead = $('<thead />');
+    $thead.append($tr);
+    headers.forEach(header => $tr.append(`<th>${header}</th>`));
+    const $tbody = $('<tbody />');
+    $table.append($thead, $tbody);
+    $tablewrap.append($table);
+    return {
+        $table: $tablewrap,
+        $tbody
+    };
+}
+
