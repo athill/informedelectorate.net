b1396de203284592613a66ac841306ecc26ed9c8
Andy Hill <andy@andyhill.us>
2017-02-13 18:28:08 -0500

    utils/index: fix getQueryByParameter

--- resources/assets/js/utils/index.js
+++ resources/assets/js/utils/index.js
@@ -8,13 +8,13 @@ export const formatDate = date => {
 	return moment(date).format("MMMM Do YYYY, h:mm:ss a");
 }
 
-export function getParameterByName(name, url) {
-    if (!url) {
-      url = window.location.href;
+export function getParameterByName(name, query) {
+    if (!query) {
+      query = window.location.search;
     }
     name = name.replace(/[\[\]]/g, "\\$&");
     var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-        results = regex.exec(url);
+        results = regex.exec(query);
     if (!results) return null;
     if (!results[2]) return '';
     return decodeURIComponent(results[2].replace(/\+/g, " "));
