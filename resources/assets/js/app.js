3a0640dde1a4b5ea050bb53345921bcfb06675f0
Andy Hill <andy@andyhill.us>
2017-10-30 17:49:58 -0400

    pwa: fix service worker path 2

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,8 +1,10 @@
 require('./bootstrap');
 
+import swURL from "file?name=sw.js!babel!./sw";
+
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('./sw.js').then(function(registration) {
+    navigator.serviceWorker.register(swURL).then(function(registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }, function(err) {
