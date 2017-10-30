fc5e555e1ae4a774f1388b773c442267560aa0b3
Andy Hill <andy@andyhill.us>
2017-10-30 18:46:09 -0400

    pwa: move sw.js to public

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,19 +1,5 @@
 require('./bootstrap');
 
-import swURL from "file?name=sw.js!babel!./sw";
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register(swURL).then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
-
 var page = location.pathname.replace(/\/([^\/]+)(\/|$)/, '$1');
 if (page === '/') {
 	page = 'home';
