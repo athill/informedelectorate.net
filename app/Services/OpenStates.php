af99685a787f31b9581c1f1a58f66153420fbce0
Andy Hill <andy@andyhill.us>
2018-04-24 21:26:27 -0400

    Services/OpenStates: Start speccing out graphql

--- app/Services/OpenStates.php
+++ app/Services/OpenStates.php
@@ -29,3 +29,126 @@ class OpenStates extends Api {
 		return $data;
 	}	
 }
+
+/*
+graphql
+http://graphql.org/learn/introspection/ // introspection
+http://docs.openstates.org/en/latest/api/v2/ // open states api
+
+endpoint: http://alpha.openstates.org/graphql
+
+states: 
+{
+	jurisdictions {
+		edges {
+			node {
+				name
+			}
+		}
+	}
+} 
+
+bills (not all options) 
+{
+	bills(jurisdiction: "Indiana", first: 20) {
+		edges {
+			node {
+				abstracts {
+					abstract
+				}
+				actions {
+					organization {
+						name
+					}
+					description
+					date
+					classification
+				}
+				classification
+				createdAt
+				documents {
+					note
+					date
+				}
+				extras
+				fromOrganization {
+					name
+				}
+				id
+				identifier
+				legislativeSession {
+					name
+				}
+				otherIdentifiers {
+					identifier
+				}
+				otherTitles {
+					title
+				}
+				relatedBills {
+					identifier
+					relatedBill {
+						title
+					}
+				}
+				sources {
+					url
+				}
+				sponsorships {
+					person {
+						name
+					}
+				}
+				subject
+				title
+				updatedAt
+				versions {
+					
+					date
+				}
+				votes {
+					edges {
+						node {
+							billAction {
+								vote {
+									identifier
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+meta:
+queries:
+{
+	__schema {
+		queryType {
+			name
+			kind 
+			enumValues {
+				name description
+			}
+			fields {
+				name
+				description
+				isDeprecated 
+				args {
+					name
+					defaultValue
+				}
+			}
+			inputFields {
+				name
+				description
+				defaultValue
+			}
+			
+		}
+	}
+}
+
+*/
