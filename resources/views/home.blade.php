bd36daf45c09acd758c5768fc01ff2fa4db333d2
Andy Hill <athill@iu.edu>
2022-07-22 19:51:15 -0400

    fix a couple things

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,10 +6,10 @@
 <p>Unfortunately, time and resources prevented the site from reaching its aspirations.</p>
 <p> Here are some resources to stay informed:</p>
     <ul>
-        <li><a href="https://www.govtrack.us/">GovTrack.us</a><li>
+        <li><a href="https://www.govtrack.us/">GovTrack.us</a></li>
         <li><a href="https://www.vote.org/polling-place-locator/">Polling place locator</a></li>
         <li><a href="https://ballotpedia.org/Sample_Ballot_Lookup">Sample ballot lookup</a></li>
-        <li><a href="https://openstates.org/">openstates</a>/li>
+        <li><a href="https://openstates.org/">openstates</a></li>
 
     </ul>
 </p>
